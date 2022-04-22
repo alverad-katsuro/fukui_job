fb806e170e545787a9a2fa20b120c59dd34ef7f8
Alfredo Gabriel <amgabriel1@hotmail.com>
2022-04-22 00:01:39 -0300

    testando func calcula_freq

--- calcula_fukui.py
+++ calcula_fukui.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import argparse
+import argparse, os
 from multiprocessing import Process, cpu_count
 import numpy as np
 import pandas as pd
-import os
 from uuid import uuid4
 
 
@@ -12,13 +11,13 @@ from uuid import uuid4
 ### Stagio 2 -> calcula fukui
 def generate_conf(smiles, job_name):
   global args
-  print("\033[1;93mGerando PDB\n\033[0;0m")
+  print("\033[1;34mGerando PDB\n\033[0;30m", flush=True)
   os.system(f"obabel -:'{smiles}' -o pdb -O {args['storage_path']}/{job_name}/pdb/inicio.pdb --gen3d --ff GAFF -h -minimize")
-  print("\033[1;93mGerando Conformeros\n\033[0;0m")
+  print("\033[1;34mGerando Conformeros\n\033[0;30m", flush=True)
   os.system(f"obabel {args['storage_path']}/{job_name}/pdb/inicio1.pdb -O {args['storage_path']}/{job_name}/pdb/conformeros.pdb --conformer --nconf 10 --writeconformers")
   conformeros = np.array_split(open(f"{args['storage_path']}/{job_name}/pdb/conformeros.pdb", "r").read().split("\n")[:-1], args["conf_num"])
   confor_index = 0
-  print("\033[1;93mEscrevendo os Conformeros\n\033[0;0m")
+  print("\033[1;34mEscrevendo os Conformeros\n\033[0;30m", flush=True)
   while (len(conformeros) > 0):
     conformero = conformeros.pop()
     with open(f"{args['storage_path']}/{job_name}/pdb/0_stage_conformero_{confor_index}.pdb", "w") as pdb:
@@ -33,49 +32,58 @@ def generate_conf(smiles, job_name):
       com.write("--Link1--\n")
       com.write(f"%NProcShared={args['threads']}\n")
       com.write(f"%Oldchk=1_stage_conformero_{confor_index}.chk\n")
-      com.write(f"%Chk=1_stage_conformero_{confor_index}_solv.chk\n")
       com.write("#n m062x/6-311G(d,p) geom=check scrf=(SMD,solvent=water) scf=maxcycle=1000 maxdisk=200Gb\n")
       com.write(f"\n {job_name}_solv\n")
       com.write("\n0  1\n")
     confor_index += 1
-  print("\033[1;93mVá na pasta jobs e execute o 'run_all.py' para submeter todos jobs no slurm\n\033[0;0m")
+  print("\033[1;32mVá na pasta jobs e execute o 'run_all.py' para submeter todos jobs no slurm\n\033[0;30m", flush=True)
 
 def rankeamento():
-  print("\033[1;93mRankeando os Conformeros (PRODUCT - REAGENT)\n\033[0;0m")
+  print("\033[1;34mRankeando os Conformeros (PRODUCT - REAGENT)\n\033[0;30m", flush=True)
   log_gaus = os.popen("ls 1_stage_conformero_*.log").read().split()
+  if len(log_gaus) == 0:
+    log_gaus = os.popen("ls 1_stage/1_stage_conformero_*.log").read().split()
   energias = {}
   while (len(log_gaus) > 0):
     nome_arquivo = log_gaus.pop()
     energias[nome_arquivo] = []
-    grep = os.popen(f"grep 'HF=' {nome_arquivo}").readlines()
-    for linhas in grep:
-      for elemento in linhas.split("\\"):
-        if "HF=" in elemento:
-          energias[nome_arquivo].append(elemento)
+    grep = os.popen(f"grep -A 1 'HF' {nome_arquivo}").read().split('\\')
+    for elemento in grep:
+      if "HF" in elemento:
+        energias[nome_arquivo].append(elemento.replace("\n ", ""))
     try:
       energias[nome_arquivo] = (float(energias[nome_arquivo][1][3:]) - float(energias[nome_arquivo][0][3:])) / 627.5
     except IndexError:
-      print("Erro no rankeamento, um dos calculos não rodou(product ou reagent)")
-      os.system("echo 'export STATUS_FREQ = 'ERROR_RANK'' > control/variables_status_freq.variables")
-      exit(1)
-  rank = 0
+      print(f"\033[1;33mErro no rankeamento do {nome_arquivo}, um dos calculos não rodou(product ou reagent)\033[0;30m", flush=True)
+      energias.pop(nome_arquivo)
+  if len(energias.keys()) == 0:
+    os.environ["STATUS_FREQ"] = "ERROR_RANK"
+    exit(1)
+  else:
+    os.environ["STATUS_FREQ"] = "FALSE"
+  rank_index = 0
   ordem_melhores = sorted(energias, key=energias.get)
-  print("\033[1;93mEscrevendo o Rankeamento -> 1_stage_rank.log\n\033[0;0m")
+  print("\033[1;34mEscrevendo o Rankeamento -> 1_stage_rank.log\n\033[0;30m", flush=True)
   with open("1_stage_rank.log", "w") as rank:
+    rank.write("RANK NAME INDEX ENERGY(Kcal/mol) STATUS_FREQ\n")
     for key in ordem_melhores:
-      rank.write("RANK NAME INDEX ENERGY(Kcal/mol) STATUS_FREQ \n\n")
-      rank.write("{rank} {nome} {index} {energia}\n".format(rank = rank, index = int(key.replace('.','_').split('_')[-2]), nome = key, energia = energias[nome_arquivo]))    
-      rank += 1
-  exit(0)
+      rank.write("{rank} {nome} {index} {energia}\n".format(rank = rank_index, index = int(key.replace('.','_').split('_')[-2]), nome = key, energia = energias[nome_arquivo]))    
+      rank_index += 1
 
 def verifica_freq(nome_arquivo):
   if os.path.isfile(nome_arquivo) == True:
-    frequencias = os.popen(f"grep 'Frequencies' {nome_arquivo}").read().split("\n")
+    frequencias = os.popen(f"grep 'Frequencies' {nome_arquivo}").read().split("\n")[-1]
+    if len(frequencias) == 0:
+      ranks = pd.read_csv('1_stage_rank.log', sep=' ').set_index("RANK")
+      status_index = ranks.query("STATUS_FREQ == 'calculate'").index[0]
+      ranks.loc[status_index, "STATUS_FREQ"] = np.NaN
+      ranks.to_csv("1_stage_rank.log", sep=' ')
+      return "ERROR"
   else :
-    print("\033[1;93mArquivo de log não encontrado!.\n\033[0;0m")
+    print("\033[1;33mArquivo de log não encontrado!.\n\033[0;30m", flush=True)
     exit(1)
   matriz_verdade = []
-  print("\033[1;93mEscrevendo Frequenica -> frequencia.log.\n\033[0;0m")
+  print("\033[1;34mEscrevendo Frequenica -> frequencia.log.\n\033[0;30m", flush=True)
   with open(f"frequencia.log", "a") as log:
     log.write("{aste} Frequencia do {nome_arquivo} {aste}\n\n".format(aste=13*"#", nome_arquivo = nome_arquivo))
     log.write
@@ -94,54 +102,51 @@ def verifica_freq(nome_arquivo):
     log.write("{aste} Frequencia Inicial é {menor_freq} {aste}\n".format(aste=9*"#", menor_freq = matriz_verdade[0]))
     log.write("{aste} Frequencia Final é {maior_freq} {aste}\n".format(aste=10*"#", maior_freq = matriz_verdade[-1]))
     log.write("{aste}\n".format(aste=49*"#"))
-    if matriz_verdade[0] >= 0:
-      return True
-    else:
-      return False
+    return matriz_verdade[0]
 
 ## Preciso achar o melhor rank caso geral e criar 
 def calcula_freq():
-  ranks = pd.read_csv('1_stage_rank.log', sep=' ')
-  ranks = ranks.set_index("RANK")
-  atualiza_status = ranks.query("STATUS_FREQ == 'calculate").index
+  ranks = pd.read_csv('1_stage_rank.log', sep=' ').set_index("RANK")
+  atualiza_status = ranks.query("STATUS_FREQ == 'calculate'").index
   if len(atualiza_status) > 0:
-    print("\033[1;93mAtualizando 'log' Frequencia -> frequencia.log\n\033[0;0m")
+    print("\033[1;34mAtualizando 'log' Frequencia -> frequencia.log\n\033[0;30m", flush=True)
     status_index = atualiza_status[0]
-    if (verifica_freq(ranks.loc[status_index, "NAME"])):
+    return_freq = verifica_freq(ranks.loc[status_index, "NAME"])
+    if return_freq == 'ERROR':
+      pass
+    elif return_freq > 0:
       ranks.loc[status_index, "STATUS_FREQ"] = "Pass"
       ranks.to_csv("1_stage_rank.log", sep=' ')
-      os.system("echo 'export STATUS_FREQ = 'TRUE'' > control/variables_status_freq.variables")
-      exit(0)
+      os.environ["STATUS_FREQ"] = "TRUE"
     else:
       ranks.loc[status_index, "STATUS_FREQ"] = "Failed"
-  print("\033[1;93mCalculando Frequencia\n\033[0;0m")
+  print("\033[1;34mCalculando Frequencia\n\033[0;30m", flush=True)
   rank_usado = ranks.loc[ranks['STATUS_FREQ'].isnull()].sort_values(by='RANK')
   if len(rank_usado) == 0:
-    os.system("echo 'export STATUS_FREQ = 'NO_FREQ'' > control/variables_status_freq.variables")
-    print("Todos possuem frequencia negativa")
-    exit(0)
+    os.environ["STATUS_FREQ"] = "NO_FREQ"
+    print("\033[1;34mTodos possuem frequencia negativa\033[1;30m", flush=True)
   else:
     rank_usado = rank_usado.index[0]
   nome = ranks.loc[rank_usado, 'NAME']
   with open(f"2_stage_rank_{rank_usado}.com", "w") as com:
     com.write(f"%NProcShared={os.environ['SLURM_NTASKS']}\n")
-    com.write(f"%Oldchk=1_stage_conformero_{ranks.loc[rank_usado, 'INDEX']}.chk\n")
+    com.write(f"%Oldchk=1_stage/1_stage_conformero_{ranks.loc[rank_usado, 'INDEX']}.chk\n")
     com.write(f"%Chk=2_stage_rank_{rank_usado}.chk\n")
     com.write("# m062x/6-311G(d,p) freq=noraman Opt Pop=NBO geom=check scf=maxcycle=1000 maxdisk=100Gb\n\n")
     com.write(f" {nome}\n\n")
     com.write("0 1\n")
-  ranks.iloc[rank_usado, "STATUS_FREQ"] = 'calculate'
+  ranks.loc[rank_usado, "STATUS_FREQ"] = 'calculate'
+  os.system(f"g09 < 2_stage_rank_{rank_usado}.com > 2_stage_rank_{rank_usado}.log")
   ranks.to_csv("1_stage_rank.log", sep=' ')
-  exit(0)
 
 def calcula_fukui():
   ranks = pd.read_csv('1_stage_rank.log', sep=' ')
   ligante = ranks.query("STATUS_FREQ == 'Pass")
   if len(ligante) == 1:
-    print("\033[1;93mCalculando FUKUI\n\033[0;0m")
+    print("\033[1;34mCalculando FUKUI\n\033[0;30m", flush=True)
     with open(f"3_stage_rank_{ligante['RANK']}.com", "w") as com:
       com.write(f"%NProcShared={os.environ['SLURM_NTASKS']}\n")
-      com.write(f"%Oldchk=2_stage_rank_{ligante['RANK']}.chk\n")
+      com.write(f"%Oldchk=2_stage/2_stage_rank_{ligante['RANK']}.chk\n")
       com.write("%Chk=fk+.chk\n")
       com.write("# m062x/6-311G(d,p) SP Pop=NBO geom=check scf=maxcycle=1000 maxdisk=100Gb\n\n")
       com.write(f" {ligante['NAME']}\n\n")
@@ -161,9 +166,7 @@ def calcula_fukui():
       com.write(f" {ligante['NAME']}\n\n")
       com.write("0 2\n\n")
   elif len(ligante) > 1:
-    print("Error --- Mais de um ligante")
-  else:
-    exit(0)
+    print("\033[1;33mError --- Mais de um ligante\033[0;30m", flush=True)
 
 def cria_lanza(job_name):
   global args
@@ -176,26 +179,10 @@ def cria_lanza(job_name):
     the_file.write("\n")
     the_file.write("date; pwd;\n\n")
     the_file.write("module load gaussian/09\n\n")
-    the_file.write("export STATUS_FREQ=FALSE\n\n")
-    for confor_index in range(args["conf_num"]):
-      the_file.write(f"echo '1_stage_conformero_{confor_index}.com' &&\n")
-      the_file.write(f"g09 < 1_stage_conformero_{confor_index}.com > 1_stage_conformero_{confor_index}.log &&\n")
-    the_file.write(f"echo 'Calculando RANK'\n")
-    the_file.write("./calcula_fukui.py --ranking 1\n")
-    the_file.write("source control/variables_status_freq.variables\n")
-    the_file.write(f"echo 'Buscando frequencia não negativa'\n")
-    the_file.write("while [[ $STATUS_FREQ = 'FALSE' ]]\n")
-    the_file.write("do\n")
-    the_file.write("./calcula_fukui.py --calc-freq 1\n")
-    the_file.write("source control/variables_status_freq.variables\n")
-    the_file.write("done\n")
-    the_file.write("if [[ $STATUS_FREQ = 'TRUE' ]]\n")
-    the_file.write("then\n")
-    the_file.write(f"echo 'Achei a frequencia'\n")
-    the_file.write(f"echo 'Calculando Fukui'\n")
-    the_file.write("./calcula_fukui.py --calc-fukui 1\n")
-    the_file.write("fi\n")
-    the_file.write("date\n")
+    for env in args.keys():
+      the_file.write(f"export {env}={args[env]}\n")
+    the_file.write("./calcula_fukui.py --run-job 1\n\n")
+    the_file.write("date;")
     the_file.write("echo \"End job\"\n\n")
 
 def create_run_all():
@@ -213,7 +200,7 @@ def create_run_all():
     run_all.write("    pass\n")
   os.system(f"chmod +x {args['storage_path']}/run_all.py")
 
-def run_jobs():
+def submete_jobs():
   global args
   os.chdir(f"{os.environ['PWD']}/{args['storage_path']}")
   os.system("./run_all.py")
@@ -224,12 +211,30 @@ def sub_rotina(smiles):
     job_name = str(uuid4())[:8]
     if not os.path.exists("{storage_path}/{job_name}/pdb".format(storage_path = args["storage_path"], job_name = job_name)):
       os.makedirs("{storage_path}/{job_name}/pdb".format(storage_path = args["storage_path"], job_name = job_name))
-    if not os.path.exists("{storage_path}/{job_name}/control".format(storage_path = args["storage_path"], job_name = job_name)):
-      os.makedirs("{storage_path}/{job_name}/control".format(storage_path = args["storage_path"], job_name = job_name))
     generate_conf(smiles.pop(), job_name)
     cria_lanza(job_name)
     os.system(f"echo '{job_name}' >> {args['storage_path']}/jobs_index.txt")
     os.system(f"cp {__file__} {args['storage_path']}/{job_name}/")
+
+def run_job():
+  for confor_index in range(int(os.environ["conf_num"])):
+    print(f"\033[1;34mStart 1_stage_conformero_{confor_index}.com\033[0;30m", flush=True)
+    os.system(f"g09 < 1_stage_conformero_{confor_index}.com > 1_stage_conformero_{confor_index}.log")
+  print("\033[1;34mCalculando RANK\033[0;30m", flush=True)
+  rankeamento()
+  if not os.path.exists("1_stage"):
+      os.makedirs("1_stage")
+  os.system("mv 1_stage_conformero_* 1_stage")
+  while (os.environ["STATUS_FREQ"] == "FALSE"):
+    calcula_freq()
+  if not os.path.exists("2_stage"):
+      os.makedirs("2_stage")
+  os.system("mv 2_stage_rank_* 2_stage")
+  if os.environ["STATUS_FREQ"] == "TRUE":
+    print("\033[1;34mAchei a frequencia\033[0;30m", flush=True)
+    print("\033[1;34mCalculando Fukui\033[0;30m", flush=True)
+    calcula_fukui()
+  exit(0)
 
 def main():
   global args
@@ -240,7 +245,7 @@ def main():
     else:
       smiles = [smiles]
   except:
-    print("\033[1;93mVerifique se o arquivo existe!!!\033[0;81m")
+    print("\033[1;33mVerifique se o arquivo existe!!!\033[0;30m", flush=True)
     exit(1)
   if not os.path.exists("{storage_path}".format(storage_path = args["storage_path"])):
       os.makedirs("{storage_path}".format(storage_path = args["storage_path"]))
@@ -257,18 +262,18 @@ def main():
   exit(0)
 
 def creditos():
-  print("\033[1;35m{aste}".format(aste=('*'*69)))
-  print("\033[1;35m{aste} \033[1;93mIdealizador  - Jerônimo Lameira \033[1;35m{aste}".format(aste=('*'*18)))
-  print("\033[1;35m{aste} \033[1;93mCodificação  - Alfredo Gabriel  \033[1;35m{aste}".format(aste=('*'*18)))
-  print("\033[1;35m{aste} \033[1;93mFluxo Lógica - Carlos Gabriel   \033[1;35m{aste}".format(aste=('*'*18)))
-  print("\033[1;35m{aste} \033[1;93mFluxo Lógica - Clauber Henrique \033[1;35m{aste}".format(aste=('*'*18)))
-  print("\033[1;35m{aste}".format(aste=('*'*69)))
+  print("\033[1;35m{aste}".format(aste=('*'*69)), flush=True)
+  print("\033[1;35m{aste} \033[1;93mIdealizador  - Jerônimo Lameira \033[1;35m{aste}".format(aste=('*'*18)), flush=True)
+  print("\033[1;35m{aste} \033[1;93mCodificação  - Alfredo Gabriel  \033[1;35m{aste}".format(aste=('*'*18)), flush=True)
+  print("\033[1;35m{aste} \033[1;93mFluxo Lógica - Carlos Gabriel   \033[1;35m{aste}".format(aste=('*'*18)), flush=True)
+  print("\033[1;35m{aste} \033[1;93mFluxo Lógica - Clauber Henrique \033[1;35m{aste}".format(aste=('*'*18)), flush=True)
+  print("\033[1;35m{aste}".format(aste=('*'*69)), flush=True)
   exit(0)
 
 def help_full():
-  print("A ser desenvolvido")
-  print("Funções marcadas com OBS são chamadas pelo slurm")
-  print("Só as use se ja tiver completado as etapas anteriores")
+  print("\033[1;32mA ser desenvolvido", flush=True)
+  print("Funções marcadas com OBS são chamadas pelo slurm", flush=True)
+  print("Só as use se ja tiver completado as etapas anteriores\033[0;30m", flush=True)
   exit(0)
 
 if __name__ == "__main__":
@@ -283,7 +288,8 @@ if __name__ == "__main__":
     parser.add_argument("--ranking", "-r", help="Realiza o Rankemanto com base no (Produto - Reagente)/627.5 \033[1;93m(1 -> Sim) OBS:Função Interna\033[0;0m", type=int)
     parser.add_argument("--calc-freq", "-cfreq", help="Calcula a Frequencia \033[1;93m(1 -> Sim) OBS:Função Interna\033[0;0m", type=int)
     parser.add_argument("--calc-fukui", "-cfukui", help="Calcula o índice de Fukui \033[1;93m(1 -> Sim) OBS:Função Interna\033[0;0m", type=int)
-    parser.add_argument("--run-jobs", "-rj", help="Submete todos os jobs na pasta \033[1;93m(1 -> Sim) OBS:Função Interna\033[0;0m", type=int)
+    parser.add_argument("--sub-jobs", "-sj", help="Submete todos os jobs na pasta \033[1;93m(1 -> Sim) OBS:Função Interna\033[0;0m", type=int)
+    parser.add_argument("--run-job", "-rj", help="Descreve algumas flags \033[1;93m(1 -> Sim) OBS:Função Interna\033[0;0m", type=int)
     parser.add_argument("--help-full", "-hf", help="Descreve algumas flags \033[1;93m(1 -> Sim) OBS:Função Interna\033[0;0m", type=int)
     args = {k: v for k, v in vars(parser.parse_args()).items()}
     if not args["extract_freq"] is None:
@@ -305,8 +311,6 @@ if __name__ == "__main__":
       args["queue_job"] = "cpu"
     if args["time_job"] is None:
       args["time_job"] = "5-00:00:00"
-    if args["run_jobs"] == True:
-      run_jobs()
     if args["calc_freq"] == True:
       calcula_freq()
     if args["calc_fukui"] == True:
@@ -315,6 +319,10 @@ if __name__ == "__main__":
       rankeamento()
     if args["help_full"] == True:
       help_full()
+    if args["run_job"] == True:
+      run_job()
+    if args["sub_job"] == True:
+      submete_jobs()
     if not args["smiles_file"] is None:
       main()
     creditos()
